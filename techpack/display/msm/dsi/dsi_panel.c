e9e55aba0ff99b341f511de10cf9305e0cedc4d7
jzc233 <63482703+jzc233@users.noreply.github.com>
2025-04-12 17:37:04 +0800

    Update dsi_panel.c

--- techpack/display/msm/dsi/dsi_panel.c
+++ techpack/display/msm/dsi/dsi_panel.c
@@ -25,7 +25,7 @@
 #include <asm/uaccess.h>
 #include <asm/fcntl.h>
 
-#include <drm/drm_notifier.h>
+#include <drm/drm_notifier_mi.h>
 #include <soc/qcom/socinfo.h>
 
 #define DSI_READ_WRITE_PANEL_DEBUG 1

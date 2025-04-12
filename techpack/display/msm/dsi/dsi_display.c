6e90a10aa21ea0c1df415fe5e9dea111b35a8f8c
jzc233 <63482703+jzc233@users.noreply.github.com>
2025-04-12 16:51:27 +0800

    Update dsi_display.c

--- techpack/display/msm/dsi/dsi_display.c
+++ techpack/display/msm/dsi/dsi_display.c
@@ -9,7 +9,7 @@
 #include <linux/of_gpio.h>
 #include <linux/err.h>
 
-#include <drm/drm_notifier.h>
+#include <drm/drm_notifier_mi.h>
 #include "msm_drv.h"
 #include "sde_connector.h"
 #include "msm_mmu.h"
